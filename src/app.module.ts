a4e7c5a01c1e9e4001cc220ed103d6ba1a79a985
Alfredo <alfredoturvedey@gmail.com>
2026-05-21 23:55:02 -0400

    feat: create modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { ProjectsModule } from './modules/projects/projects.module';
+import { TasksModule } from './modules/tasks/tasks.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProjectsModule, TasksModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, Column } from 'typeorm';
+import { BaseEntity } from '../../../shared/entity/base-entity';
+
+@Entity('users')
+export class User extends BaseEntity {
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  lastName: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  password: string;
+}
